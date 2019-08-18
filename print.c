ee9e2c2155d44ac21f62885e1285b7b83e99e5ee
ganimaedes <jemedal1000@gmx.fr>
2019-08-17 20:43:43 -0400

    print.c

--- print.c
+++ print.c
@@ -19,14 +19,27 @@ typedef struct {
 
 int debug_mode = 1;
 
-//int sz = 14;
 int sz = 29;
 static char *entry[] = { "array.c", "array.h", "CMakeLists.txt", "coc-settings.json",
                          "main.c", "main2.c", "main3.c", "README.md", "scr.c", "scr.h",
                          "screen.c", "settings.json", "test", "test.c",
-                         "boot", "grub", "memtest86+.bin", "memtest86+.elf", 
-                         "memtest86+_multiboot.bin", "grubenv", "unicode.pf2", "usr", "bin", 
+                         "boot", "grub", "memtest86+.bin", "memtest86+.elf",
+                         "memtest86+_multiboot.bin", "grubenv", "unicode.pf2", "usr", "bin",
                          "alsaloop", "alsamixer", "alsatplg", "alsaucm", "amidi", "amixer" };
+
+void highlight(char *entry[], int *maxlen, int *pos, int *len)
+{
+    int i;
+    for (i = 0; i < *maxlen; ++i) {
+        if (i < *len) {
+            printf("%s%c", bg_cyan, entry[*pos][i]);
+        } else if (i == *maxlen - 1) {
+            printf(" %s", bg_reset);
+        } else {
+            printf(" %s", bg_cyan);
+        }
+    }
+}
 
 int main(void)
 {
@@ -48,14 +61,25 @@ int main(void)
 
     Scroll s;
     s.array_size = sz;
-    s.n_to_print = 10;
+    s.n_to_print = 10; // w_1.y_size;
     s.n_upper_t = pos;
     s.n_lower_t = sz - s.n_to_print;
 
     s.pos_upper_t = pos;
     s.pos_lower_t = s.n_to_print - 1;
 
-    for (int i = 0; i < s.n_to_print; ++i) {
+
+    int SIZE = 0;
+    if (s.n_to_print < sz) {
+        SIZE = s.n_to_print;
+    } else {
+        SIZE = sz - 1;
+    }
+
+    int i = 0;
+    int y = 0;
+
+    for (i = 0; i < SIZE; ++i) {
 
         gotoyx(i + w_1.y_beg, w_1.x_beg);
         if (i == 0) {
@@ -74,25 +98,17 @@ int main(void)
         }
     }
 
-    int previous_pos = pos;
-    int y = 0;
+
 
     while (1) {
 
-        /*
-        if ((c == KEY_DOWN || c == DN) && pos - 1 == s.pos_lower_t && s.pos_lower_t < sz - 1 && s.n_lower_t >= 0) { 
-
-        } else if ((c == KEY_UP || c == UP) && pos + 1 == s.pos_upper_t && s.pos_upper_t > 0) {
-
-        }
-        */
         if (debug_mode) {
             gotoyx(2, (w_1.x_size / 2) - 20);
             del_ncharc2right(42);
             printf("pos: %d, s.n_upper_t: %d, s.n_lower_t: %d",
                     pos, s.n_upper_t, s.n_lower_t);
             gotoyx(3, (w_1.x_size / 2) - 20);
-            del_ncharc2right(48);
+            del_ncharc2right(56);
             printf("s.pos_upper_t: %d, s.pos_lower_t: %d, s.n_to_print: %d",
                     s.pos_upper_t, s.pos_lower_t, s.n_to_print);
 
@@ -102,7 +118,7 @@ int main(void)
 
             gotoyx(w_1.y_beg + s.n_to_print - 1, (w_1.x_size / 2) - 20);
             del_ncharc2right(54);
-            printf("__________________ s.pos_lower_t: %d, s.n_lower_t: %d", 
+            printf("__________________ s.pos_lower_t: %d, s.n_lower_t: %d",
                     s.pos_lower_t, s.n_lower_t);
 
             gotoyx(w_1.y_size - 1, w_1.x_size - 15);
@@ -121,144 +137,68 @@ int main(void)
             break;
         } else if (c == KEY_UP || c == UP) {
             if (pos > 0) {
-                int i = 0;
                 --pos;
+                y = pos - s.pos_upper_t + w_1.y_beg;
                 if (pos >= s.pos_upper_t && pos < s.pos_lower_t) { // NO SCROLL
-                    
-                    //gotoyx(pos - s.pos_upper_t + w_1.y_beg, w_1.x_beg);
-                    //del_ncharc2right(maxlen);
 
-                    /* *** */
-                    y = pos - s.pos_upper_t + w_1.y_beg;
-                    gotoyx(pos - s.pos_upper_t + w_1.y_beg + 1, w_1.x_beg);
+                    gotoyx(y + 1, w_1.x_beg);
                     del_ncharc2right(maxlen);
-                    //printf("%s", entry[pos + s.pos_upper_t + 1]);
                     printf("%s", entry[pos + 1]);
-                    gotoyx(pos - s.pos_upper_t + w_1.y_beg, w_1.x_beg);
-                    del_ncharc2right(maxlen);
-                    
-                    //len = strlen(entry[s.pos_upper_t + pos]);
-                    len = strlen(entry[pos]);
-                    /* *** */
-
-                    for (i = 0; i < maxlen; ++i) {
-                        if (i < len) {
-                            //printf("%s%c", bg_cyan, entry[s.pos_upper_t + pos][i]);
-                            printf("%s%c", bg_cyan, entry[pos][i]);
-                        } else if (i == maxlen - 1) {
-                            printf(" %s", bg_reset);
-                        } else {
-                            printf(" %s", bg_cyan);
-                        }
-                    }
-
+                    gotoyx(y, w_1.x_beg);
 
                 } else if (pos <= s.pos_upper_t && s.pos_upper_t > 0) {
-                    //--pos;
                     --s.pos_upper_t;
                     --s.pos_lower_t;
                     ++s.n_lower_t;
                     --s.n_upper_t;
 
-                    /* **************************************************** */
                     // reprint all 10 decalees de 1 a partir de pos
-                    int j = pos; 
-                    for (i = 0; i < s.n_to_print ; ++i, ++j) {
+                    int j = pos;
+                    for (i = 0; i < SIZE; ++i, ++j) {
                         gotoyx(i + w_1.y_beg, w_1.x_beg);
                         del_ncharc2right(maxlen);
                         printf("%s", entry[j]);
                     }
-                     
+
                     gotoyx(pos - s.n_upper_t + w_1.y_beg, w_1.x_beg);
                     del_ncharc2right(maxlen);
-                    
-                    
-                    len = strlen(entry[pos]);
-                     
-                    for (i = 0; i < maxlen; ++i) {
-                        if (i < len) {
-                            printf("%s%c", bg_cyan, entry[pos][i]);
-                        } else if (i == maxlen - 1) {
-                            printf(" %s", bg_reset);
-                        } else {
-                            printf(" %s", bg_cyan);
-                        }
-                    }
-                    
 
-
-                    /* **************************************************** */
-
-
-
-
-                
                 }
+                len = strlen(entry[pos]);
+                highlight(entry, &maxlen, &pos, &len);
             }
         } else if (c == KEY_DOWN || c == DN) {
             if (pos < sz - 1) {
-                int i = 0;
                 ++pos;
+                y = pos - s.pos_upper_t + w_1.y_beg;
                 if (pos > s.pos_upper_t && pos <= s.pos_lower_t) { // NO SCROLL
-                    
-                    y = pos;
 
-                    gotoyx(pos - s.pos_upper_t + w_1.y_beg - 1, w_1.x_beg);
+
+                    gotoyx(y - 1, w_1.x_beg);
                     del_ncharc2right(maxlen);
-                    //printf("%s", entry[pos + s.pos_upper_t - 1]);
                     printf("%s", entry[pos  - 1]);
-                    
-                    //len = strlen(entry[s.pos_upper_t + pos]);
-                    len = strlen(entry[pos]);
+                    gotoyx(y, w_1.x_beg);
 
-                    gotoyx(pos -  s.pos_upper_t + w_1.y_beg, w_1.x_beg);
-                    for (i = 0; i < maxlen; ++i) {
-                        if (i < len) {
-                            //printf("%s%c", bg_cyan, entry[s.pos_upper_t + pos][i]);
-                            printf("%s%c", bg_cyan, entry[pos][i]);
-                        } else if (i == maxlen - 1) {
-                            printf(" %s", bg_reset);
-                        } else {
-                            printf(" %s", bg_cyan);
-                        }
-                    }
-                    
-                } else if (pos - 1>= s.pos_lower_t) {// pos == s.pos_lower_t &&  pos > s.n_to_print - 1
-
-
-                    //++pos;
+                } else if (pos - 1 >= s.pos_lower_t) {
 
                     ++s.n_upper_t;
                     s.pos_upper_t = s.n_upper_t;
                     --s.n_lower_t;
                     ++s.pos_lower_t;
 
-                    for (i = 0; i < s.n_to_print; ++i) {
+                    for (i = 0; i < SIZE; ++i) {
                         gotoyx(i + w_1.y_beg, w_1.x_beg);
                         del_ncharc2right(maxlen);
-                        printf("%s", entry[s.n_upper_t + i ]);
+                        printf("%s", entry[s.n_upper_t + i]);
                     }
                     gotoyx(s.n_to_print + w_1.y_beg - 1, w_1.x_beg);
                     del_ncharc2right(maxlen);
 
-                    //len = strlen(entry[pos + 1]);
-                    len = strlen(entry[pos]);
-
-                    for (i = 0; i < maxlen; ++i) {
-                        if (i < len) {
-                            //printf("%s%c", bg_cyan, entry[pos + 1][i]);
-                            printf("%s%c", bg_cyan, entry[pos][i]);
-                        } else if (i == maxlen - 1) {
-                            printf(" %s", bg_reset);
-                        } else {
-                            printf(" %s", bg_cyan);
-                        }
-                    }
-                } 
-
-
-
+                }
+                len = strlen(entry[pos]);
+                highlight(entry, &maxlen, &pos, &len);
             }
+
         }
     }
     printf("\x1b[2J\x1b[H\x1b[?1049l");
